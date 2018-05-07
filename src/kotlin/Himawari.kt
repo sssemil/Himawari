788c7bebe1c951cfaa303c10f10a5d24fafb7ca4
Emil Suleymanov <emil.suleymanov@tum.de>
2018-05-07 04:23:22 +0200

    Refactor into a Himawari class

---
+++ src/kotlin/Himawari.kt
@@ -0,0 +1,147 @@
+import com.google.gson.GsonBuilder
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.runBlocking
+import java.awt.image.BufferedImage
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.imageio.ImageIO
+
+class Himawari(var checkMobileNet: Boolean = false,
+               var singleLoop: Boolean = false,
+               var setDesktopBg: Boolean = false,
+               var setLockScreenBg: Boolean = false,
+               var outFileName: String? = null,
+               var level: Int = 4,
+               var delay: Int = 1000,
+               var rootFolder: File = File("./")) {
+    private var previousInfo: HimawariLatest? = null
+    private val gson = GsonBuilder().setDateFormat("yyyy-mm-dd HH:mm:ss").create()
+
+    fun run() = runBlocking {
+        while (true) {
+            val latestInfo = getLatestInfo()
+
+            Logger.i("LatestInfo: ${latestInfo.toString()}")
+
+            if (latestInfo?.equals(previousInfo) == false) {
+                previousInfo = latestInfo
+                if (checkMobileNet && isOnMobile()) {
+                    val matrix: Array<Array<Deferred<BufferedImage>>> = Array(level, { x ->
+                        Array(level, { y ->
+                            async {
+                                getImage(level, latestInfo.date, x, y)
+                            }
+                        })
+                    })
+
+                    val type = BufferedImage.TYPE_3BYTE_BGR
+                    val chunkWidth = matrix[0][0].await().width
+                    val chunkHeight = matrix[0][0].await().height
+
+                    val finalImg = BufferedImage(chunkWidth * matrix.size, chunkHeight * matrix[0].size, type)
+
+                    matrix.forEachIndexed({ x, array ->
+                        array.forEachIndexed({ y, chunk ->
+                            chunk.await().let {
+                                finalImg.createGraphics().drawImage(it, it.width * x, it.height * y, null)
+                            }
+                        })
+                    })
+
+                    rootFolder.mkdirs()
+
+                    val outFile = File(rootFolder, outFileName ?: latestInfo.file)
+
+                    Logger.i("Wring downloaded image to file: ${outFile.toURI()}")
+                    ImageIO.write(finalImg, "png", outFile)
+
+                    if (setDesktopBg) {
+                        Runtime.getRuntime().exec("gsettings set org.gnome.desktop.background picture-uri ${outFile.toURI()}")
+                    }
+
+                    if (setLockScreenBg) {
+                        Runtime.getRuntime().exec("gsettings set org.gnome.desktop.screensaver picture-uri ${outFile.toURI()}")
+                    }
+                }
+            }
+
+            if (singleLoop) {
+                Logger.i("Single loop parameter specified, exiting.")
+                break
+            }
+
+            delay(delay)
+        }
+    }
+
+    private fun getLatestInfo(): HimawariLatest? {
+        val obj = URL(urlLatest)
+
+        return with(obj.openConnection() as HttpURLConnection) {
+            Logger.i("URL: $urlLatest; Response Code: $responseCode")
+
+            BufferedReader(InputStreamReader(inputStream)).use {
+                val response = StringBuilder()
+
+                var inputLine = it.readLine()
+                while (inputLine != null) {
+                    response.append(inputLine)
+                    inputLine = it.readLine()
+                }
+                return@with gson.fromJson(response.toString(), HimawariLatest::class.java)
+            }
+        }
+    }
+
+    companion object {
+        private fun isOnMobile(): Boolean {
+            val ipData = getIpInfo()
+            Logger.i("ISP: $ipData")
+
+            return !mobileIsps.contains(ipData)
+        }
+
+        private fun getImage(size: Int = 8, date: Date, x: Int, y: Int): BufferedImage {
+            val dateFormatted = SimpleDateFormat("yyyy/MM/dd/HHmmss").format(date)
+            val url = "http://himawari8.nict.go.jp/img/D531106/${size}d/550/${dateFormatted}_${x}_$y.png"
+            val obj = URL(url)
+
+            return with(obj.openConnection() as HttpURLConnection) {
+                Logger.i("URL: $url; Response Code: $responseCode")
+                ImageIO.read(inputStream)
+            }
+        }
+
+        private fun getIpInfo(): String {
+            val obj = URL(urlIpInfo)
+
+            return with(obj.openConnection() as HttpURLConnection) {
+                println("URL: $url; Response Code: $responseCode")
+
+                BufferedReader(InputStreamReader(inputStream)).use {
+                    val response = StringBuilder()
+
+                    var inputLine = it.readLine()
+                    while (inputLine != null) {
+                        response.append(inputLine)
+                        inputLine = it.readLine()
+                    }
+                    return@with response.toString()
+                }
+            }
+        }
+
+        private val mobileIsps = arrayOf("O2 Deutschland")
+        private const val urlIpInfo = "http://ip-api.com/line/?fields=isp"
+        private const val urlLatest = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json"
+    }
+}
+
+data class HimawariLatest(val date: Date, val file: String)
--- src/kotlin/Main.kt
+++ src/kotlin/Main.kt
@@ -1,21 +1,7 @@
-import com.google.gson.GsonBuilder
-import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.async
-import kotlinx.coroutines.experimental.delay
-import kotlinx.coroutines.experimental.runBlocking
 import org.apache.commons.cli.*
-import java.awt.image.BufferedImage
-import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import java.text.SimpleDateFormat
-import java.util.*
-import javax.imageio.ImageIO
 
-
-fun main(args: Array<String>) = runBlocking {
+fun main(args: Array<String>) {
     val options = Options()
 
     val levelOption = Option("l", "level", true, "increases the quality (and the size) of each tile. Possible values are 4, 8, 16, 20")
@@ -61,7 +47,7 @@ fun main(args: Array<String>) = runBlocking {
         formatter.printHelp("himawari", options)
 
         System.exit(1)
-        return@runBlocking
+        return
     }
 
     var rootFolder = File("output/")
@@ -70,136 +56,28 @@ fun main(args: Array<String>) = runBlocking {
     }
     Logger.setOutputDirectory(rootFolder)
 
-    var level = 4
+    val himawari = Himawari(checkMobileNet = cmd.hasOption(checkForModuleNetworkOption.longOpt),
+            singleLoop = cmd.hasOption(singleLoopOption.longOpt),
+            setDesktopBg = cmd.hasOption(setDesktopBgOption.longOpt),
+            setLockScreenBg = cmd.hasOption(setLockScreenBgOption.longOpt))
+
     if (cmd.hasOption(levelOption.longOpt)) {
-        level = cmd.getOptionValue(levelOption.longOpt).toInt()
+        himawari.level = cmd.getOptionValue(levelOption.longOpt).toInt()
     }
 
-    var delay = 1000
     if (cmd.hasOption(delayOption.longOpt)) {
-        delay = cmd.getOptionValue(delayOption.longOpt).toInt()
+        himawari.delay = cmd.getOptionValue(delayOption.longOpt).toInt()
     }
 
-    while (true) {
-        val latestInfo = getLatestInfo()
-
-        Logger.i("LatestInfo: ${latestInfo.toString()}")
-
-        if (latestInfo?.equals(previousInfo) == false) {
-            previousInfo = latestInfo
-            if (cmd.hasOption(checkForModuleNetworkOption.longOpt) && isOnMobile()) {
-                val matrix: Array<Array<Deferred<BufferedImage>>> = Array(level, { x ->
-                    Array(level, { y ->
-                        async {
-                            getImage(level, latestInfo.date, x, y)
-                        }
-                    })
-                })
-
-                val type = BufferedImage.TYPE_3BYTE_BGR
-                val chunkWidth = matrix[0][0].await().width
-                val chunkHeight = matrix[0][0].await().height
-
-                val finalImg = BufferedImage(chunkWidth * matrix.size, chunkHeight * matrix[0].size, type)
-
-                matrix.forEachIndexed({ x, array ->
-                    array.forEachIndexed({ y, chunk ->
-                        chunk.await().let {
-                            finalImg.createGraphics().drawImage(it, it.width * x, it.height * y, null)
-                        }
-                    })
-                })
-
-                rootFolder.mkdirs()
-
-                val outFile = File(rootFolder,
-                        if (cmd.hasOption(outFileNameOption.longOpt))
-                            cmd.getOptionValue(outFileNameOption.longOpt)
-                        else
-                            latestInfo.file)
-
-                Logger.i("Wring downloaded image to file: ${outFile.toURI()}")
-                ImageIO.write(finalImg, "png", outFile)
-
-                if (cmd.hasOption(setDesktopBgOption.longOpt)) {
-                    Runtime.getRuntime().exec("gsettings set org.gnome.desktop.background picture-uri ${outFile.toURI()}")
-                }
-
-                if (cmd.hasOption(setLockScreenBgOption.longOpt)) {
-                    Runtime.getRuntime().exec("gsettings set org.gnome.desktop.screensaver picture-uri ${outFile.toURI()}")
-                }
-            }
-        }
-
-        if (cmd.hasOption(singleLoopOption.longOpt)) {
-            Logger.i("${singleLoopOption.longOpt} parameter specified, exiting.")
-            break
-        }
-
-        delay(delay)
+    if (cmd.hasOption(outFileNameOption.longOpt)) {
+        himawari.outFileName = cmd.getOptionValue(outFileNameOption.longOpt)
     }
-}
 
-private fun isOnMobile(): Boolean {
-    val ipData = getIpInfo()
-    Logger.i("ISP: $ipData")
-
-    return !mobileIsps.contains(ipData)
-}
-
-private fun getImage(size: Int = 8, date: Date, x: Int, y: Int): BufferedImage {
-    val dateFormatted = SimpleDateFormat("yyyy/MM/dd/HHmmss").format(date)
-    val url = "http://himawari8.nict.go.jp/img/D531106/${size}d/550/${dateFormatted}_${x}_$y.png"
-    val obj = URL(url)
-
-    return with(obj.openConnection() as HttpURLConnection) {
-        Logger.i("URL: $url; Response Code: $responseCode")
-        ImageIO.read(inputStream)
+    if (cmd.hasOption(outDirOption.longOpt)) {
+        himawari.rootFolder = File(cmd.getOptionValue(outDirOption.longOpt))
     }
+
+    Logger.setOutputDirectory(himawari.rootFolder)
+
+    himawari.run()
 }
-
-private fun getLatestInfo(): HimawariLatest? {
-    val obj = URL(urlLatest)
-
-    return with(obj.openConnection() as HttpURLConnection) {
-        Logger.i("URL: $urlLatest; Response Code: $responseCode")
-
-        BufferedReader(InputStreamReader(inputStream)).use {
-            val response = StringBuilder()
-
-            var inputLine = it.readLine()
-            while (inputLine != null) {
-                response.append(inputLine)
-                inputLine = it.readLine()
-            }
-            return@with gson.fromJson(response.toString(), HimawariLatest::class.java)
-        }
-    }
-}
-
-private fun getIpInfo(): String {
-    val obj = URL(urlIpInfo)
-
-    return with(obj.openConnection() as HttpURLConnection) {
-        println("URL: $url; Response Code: $responseCode")
-
-        BufferedReader(InputStreamReader(inputStream)).use {
-            val response = StringBuilder()
-
-            var inputLine = it.readLine()
-            while (inputLine != null) {
-                response.append(inputLine)
-                inputLine = it.readLine()
-            }
-            return@with response.toString()
-        }
-    }
-}
-
-private val gson = GsonBuilder().setDateFormat("yyyy-mm-dd HH:mm:ss").create()
-private val mobileIsps = arrayOf("O2 Deutschland")
-private const val urlIpInfo = "http://ip-api.com/line/?fields=isp"
-private const val urlLatest = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json"
-private var previousInfo: HimawariLatest? = null
-
-data class HimawariLatest(val date: Date, val file: String)
