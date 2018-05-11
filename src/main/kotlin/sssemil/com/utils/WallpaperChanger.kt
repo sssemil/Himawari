50dc7e3b41d6d6dd400edbff36f954246eae32dd
Emil Suleymanov <emil.suleymanov@tum.de>
2018-05-11 02:38:40 +0200

    Support more environments

--- src/main/kotlin/sssemil/com/utils/WallpaperChanger.kt
+++ src/main/kotlin/sssemil/com/utils/WallpaperChanger.kt
@@ -5,46 +5,97 @@ import java.io.File
 
 class WallpaperChanger {
 
-    private val osName = System.getProperty("os.name")
+    private val detectedEnv = EnvDetector.environment
 
     fun setDesktop(file: File) {
-        when {
-            osName.startsWith("Windows") -> {
+        when (detectedEnv) {
+            EnvDetector.Env.WINDOWS -> {
                 val result = SPI.setDesktopWallpaper(file)
                 Logger.i("Setting desktop wallpaper result: $result")
             }
-            osName.startsWith("Linux") -> {
-                // TODO not all Linux is gnome
-                Runtime.getRuntime().exec("gsettings setDesktop org.gnome.desktop.background picture-uri ${file.toURI()}")
+            EnvDetector.Env.GNOME,
+            EnvDetector.Env.UNITY,
+            EnvDetector.Env.CINNAMON,
+            EnvDetector.Env.PANTHEON,
+            EnvDetector.Env.GNOME_CLASSIC -> {
+                if (detectedEnv == EnvDetector.Env.UNITY) {
+                    Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.background", "draw-background", "false"))
+                }
+
+                Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.background", "picture-uri", file.toURI().toString()))
+                Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.background", "picture-options", "scaled"))
+                Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.background", "primary-color", "#000000"))
+
+                if (detectedEnv == EnvDetector.Env.UNITY) {
+                    Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.background", "draw-background", "true"))
+                }
             }
-            else -> onUnsupportedDesktop(osName)
+            EnvDetector.Env.MATE -> {
+                Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.mate.background", "picture-filename", file.absolutePath))
+            }
+            EnvDetector.Env.I3 -> {
+                Runtime.getRuntime().exec(arrayOf("feh", "--bg-max", file.absolutePath))
+            }
+            EnvDetector.Env.XFCE4 -> {
+                val process = Runtime.getRuntime().exec("xfconf-query --channel xfce4-desktop --list | grep last-image")
+                process.inputStream.bufferedReader().lines().forEach {
+                    it.split(" ").forEach { display ->
+                        Runtime.getRuntime().exec(arrayOf("xfconf-query", "--channel", "xfce4-desktop", "--property", display, "--set", file.absolutePath))
+                    }
+                }
+            }
+            EnvDetector.Env.LXDE -> {
+                Runtime.getRuntime().exec(arrayOf("pcmanfm", "--set-wallpaper", file.absolutePath, "--wallpaper-mode=fit"))
+            }
+            EnvDetector.Env.DARWIN -> {
+                Runtime.getRuntime().exec(arrayOf(
+                        "osascript", "-e",
+                        "tell application \"System Events\"\n" +
+                                "set theDesktops to a reference to every desktop\n" +
+                                "repeat with aDesktop in theDesktops\n" +
+                                "set the picture of aDesktop to \"${file.absolutePath}\"\nend repeat\nend tell"))
+
+            }
+            else -> {
+                if (hasProgram("feh")) {
+                    Logger.i("Using 'feh'...")
+                    val processBuilder = ProcessBuilder()
+                    processBuilder.environment()["DISPLAY"] = ":0"
+                    processBuilder.command("feh", "--bg-max", file.absolutePath).start().waitFor()
+                } else {
+                    onUnsupportedDesktop()
+                }
+            }
         }
     }
+
+    private fun hasProgram(program: String) =
+            Runtime.getRuntime().exec(arrayOf("which", "--", program)).waitFor() == 0
 
     fun setLockScreen(file: File) {
-        when {
-            osName.startsWith("Windows") -> {
-                onUnsupportedLockScreen(osName)
+        when (detectedEnv) {
+            EnvDetector.Env.GNOME,
+            EnvDetector.Env.UNITY,
+            EnvDetector.Env.CINNAMON,
+            EnvDetector.Env.PANTHEON,
+            EnvDetector.Env.GNOME_CLASSIC -> {
+                Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.screensaver", "picture-uri", file.toURI().toString()))
+                Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.screensaver", "picture-options", "scaled"))
+                Runtime.getRuntime().exec(arrayOf("gsettings", "set", "org.gnome.desktop.screensaver", "primary-color", "#000000"))
             }
-            osName.startsWith("Linux") -> {
-                // TODO not all Linux is gnome
-                Runtime.getRuntime().exec("gsettings setDesktop org.gnome.desktop.screensaver picture-uri ${file.toURI()}")
-            }
-            else -> onUnsupportedLockScreen(osName)
+            else -> onUnsupportedLockScreen()
         }
     }
 
-    companion object {
-        private fun onUnsupportedDesktop(osName: String) {
-            Logger.w("Setting desktop wallpaper is not supported on $osName.")
-        }
+    private fun onUnsupportedDesktop() {
+        Logger.w("Setting desktop wallpaper is not supported on ${detectedEnv.summary()}.")
+    }
 
-        private fun onUnsupportedLockScreen(osName: String) {
-            Logger.w("Setting lock-screen wallpaper is not supported on $osName.")
-        }
+    private fun onUnsupportedLockScreen() {
+        Logger.w("Setting lock-screen wallpaper is not supported on ${detectedEnv.summary()}.")
     }
 
     init {
-        Logger.i("Running on: $osName")
+        Logger.i("Running on: ${detectedEnv.name}")
     }
 }
